0187666d2cef4ffdf5ac4accf10dbcb0f4125153
nrfarah22 <raarznrzt29@gmail.com>
2024-06-17 15:28:37 +0700

    added folder backend

--- routes/kategori.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const kategoriController = require('../controller/kategoriController');
-
-router.post('/kategori', kategoriController.createKategori);
